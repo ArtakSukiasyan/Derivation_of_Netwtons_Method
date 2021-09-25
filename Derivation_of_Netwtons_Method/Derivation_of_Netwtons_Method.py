905174488d6fcb5aa71cb4d3965497a33b3141ae
ArtakSukiasyan <armen93g@gmail.com>
2021-09-26 00:14:05 +0400

    add function F1 and F2

--- Derivation_of_Netwtons_Method/Derivation_of_Netwtons_Method.py
+++ Derivation_of_Netwtons_Method/Derivation_of_Netwtons_Method.py
@@ -2,6 +2,13 @@ import math as m
 def F(x):
     return 5*m.cos(x**2)-3*x
 
+def F1(x):
+    return -10*x*m.sin(x**2)-3
+
+def F2(x):
+    return -10*m.sin(x**2)-20*pow(x,2)*m.cos(pow(x,2))
+
+
 
 
 
