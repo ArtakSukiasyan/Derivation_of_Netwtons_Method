71ca0dc6d11dbcab643052a6b07709eb60bbaf20
ArtakSukiasyan <armen93g@gmail.com>
2021-09-26 00:16:24 +0400

    add while loop

--- Derivation_of_Netwtons_Method/Derivation_of_Netwtons_Method.py
+++ Derivation_of_Netwtons_Method/Derivation_of_Netwtons_Method.py
@@ -8,7 +8,18 @@ def F1(x):
 def F2(x):
     return -10*m.sin(x**2)-20*pow(x,2)*m.cos(pow(x,2))
 
+a=0.8;
+b=1.1;
+E=1e-4;
 
+x=a
+if(F(x)*F2(x)<0):
+    x=b
+
+while(abs(F(x))>E):
+    x=x-(F(x)/F2(x))
+
+print("x= ", x)
 
 
 
